f0c234622ee1b2e3cfce927eaf428190030bade8
Lee Jiangwei <ljwskrskr@outlook.com>
2020-05-09 00:23:49 +0800

    Fix that bug, and refactor content.js a little bit

--- calculate.js
+++ calculate.js
@@ -1,17 +1,27 @@
 chrome.storage.local.get("mapData", function (mapData) {
   chrome.storage.local.get("demands", function (demands) {
-    calculate(mapData, demands);
+    calculate(mapData.mapData, demands.demands);
   });
 });
 
 function calculate(mapData, demands) {
+  let ints = Object.assign({}, mapData);
+  for (let key in ints) {
+    ints[key] = 1;
+  }
+  console.log(ints);
   const model = {
     optimize: "num",
     opType: "min",
     constraints: demands,
     variables: mapData,
+    // ints: ints,
   };
-  const result = solver.Solve(model);
 
-  console.log(result);
+  const t0 = performance.now();
+  const result = solver.Solve(model);
+  const t1 = performance.now();
+  result.usedTime = t1 - t0;
+
+  chrome.storage.local.set({ plan: result });
 }
