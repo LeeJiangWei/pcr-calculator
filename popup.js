4acabce5889f35d98ed12c86d93ac9c365f66de5
Lee Jiangwei <ljwskrskr@outlook.com>
2020-05-09 04:55:48 +0800

    First version complete, v0.1.0

--- popup.js
+++ popup.js
@@ -7,6 +7,7 @@ calulateButton.addEventListener("click", function () {
     const plan = changes.plan.newValue;
     if (plan.feasible) {
       mountMessage("计算完成！用时 " + plan.usedTime + " 毫秒");
+      generateTable(plan);
     } else {
       mountMessage(
         "计算失败。可能的原因：" +
@@ -48,4 +49,26 @@ parseMapDropButton.addEventListener("click", function () {
 
 function mountMessage(message) {
   document.getElementById("mount").innerHTML = message;
+}
+
+function generateTable(plan) {
+  let html = "";
+  for (let key in plan) {
+    if (key === "result") {
+      html += `<tr><td>总次数</td><td>${plan[key]}</td></tr>`;
+    } else if (key === "feasible") {
+      html += `<tr><td>可行性</td><td>${plan[key]}</td></tr>`;
+    } else if (key === "bounded") {
+      html += `<tr><td>有界性</td><td>${plan[key]}</td></tr>`;
+    } else if (key === "usedTime") {
+      html += `<tr><td>用时</td><td>${plan[key]}ms</td></tr>`;
+    } else {
+      html += `<tr><td>${key}</td><td>${plan[key]}</td></tr>`;
+    }
+  }
+  html = `<table><tr><th>关卡名</th><th>建议次数</th></tr>${html}</table>`;
+
+  let table = document.createElement("div");
+  table.innerHTML = html;
+  document.getElementById("mount").appendChild(table);
 }
