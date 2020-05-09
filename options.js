7ab368acd1a9e9576d2008855692eb0477325802
Lee Jiangwei <ljwskrskr@outlook.com>
2020-05-09 18:55:21 +0800

    Add options page with no actual functions

---
+++ options.js
@@ -0,0 +1,58 @@
+let form = document.getElementsByTagName("form")[0];
+form.addEventListener("change", onFormChanged);
+window.addEventListener("load", onFormLoaded);
+
+const algorithmRadios = document.getElementsByName("algorithm");
+const multiplierRadios = document.getElementsByName("multiplier");
+const metricRadios = document.getElementsByName("metric");
+const displayIntCheckbox = document.getElementById("displayIntCheckbox");
+
+function onFormChanged() {
+  let options = {
+    algorithm: "lp",
+    metric: "farmTime",
+    multiplier: 1,
+    displayInt: false,
+  };
+
+  for (let radio of algorithmRadios) {
+    if (radio.checked) {
+      options.algorithm = radio.value;
+    }
+  }
+
+  for (let radio of metricRadios) {
+    if (radio.checked) {
+      options.metric = radio.value;
+    }
+  }
+
+  for (let radio of multiplierRadios) {
+    if (radio.checked) {
+      options.multiplier = parseInt(radio.value);
+    }
+  }
+
+  options.displayInt = displayIntCheckbox.checked;
+
+  chrome.storage.local.set({ options }, function () {
+    console.log("Options saved successfully.");
+  });
+}
+
+function onFormLoaded() {
+  chrome.storage.local.get("options", function (items) {
+    const { options } = items;
+
+    for (let radio of algorithmRadios) {
+      radio.checked = options.algorithm === radio.value;
+    }
+    for (let radio of metricRadios) {
+      radio.checked = options.metric === radio.value;
+    }
+    for (let radio of multiplierRadios) {
+      radio.checked = options.multiplier === parseInt(radio.value);
+    }
+    displayIntCheckbox.checked = options.displayInt;
+  });
+}
