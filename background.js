bfcfb1e4bc6e1108e7efe9fb290f7b94954d1c89
Lee Jiangwei <ljwskrskr@outlook.com>
2020-06-24 21:57:18 +0800

    Optimize declarativeContent rules

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.addRules([
-    {
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { hostEquals: "pcredivewiki.tw" },
-        }),
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()],
-    },
-  ]);
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "pcredivewiki.tw" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
   chrome.storage.local.set({
     options: {
       algorithm: "lp",
