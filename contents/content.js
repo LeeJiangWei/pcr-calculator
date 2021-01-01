c765b34a8dd82840632cef921c718f79f231d707
cznull <32773496+cznull@users.noreply.github.com>
2021-01-01 14:19:09 +0800

    Update content.js

--- contents/content.js
+++ contents/content.js
@@ -2,7 +2,7 @@ window.addEventListener("load", f);
 
 function f() {
   const buttons = document.querySelector(
-    "#app > div.main > div > div.item-box > div.row.mb-3 > div:nth-child(2) > div > h3 > div > div.col-12.col-lg-6.mb-3 > div"
+    "#app > div.main > div > div.item-box > div.row.mb-3 > div:nth-child(2) > div > h3 > div > div.col-12.col-lg-12.mb-3 > div"
   );
   const recipeModeButton = buttons.children[1];
   const mapDropModeButton = buttons.children[2];
@@ -136,7 +136,7 @@ function getMapData() {
 function getData() {
   if (!document.getElementsByClassName("mapDrop-table")[0]) return false;
 
-  const mapDropTable = document.getElementsByClassName("mapDrop-table")[0];
+  const mapDropTable = document.getElementsByClassName("mapDrop-table")[1];
   const tableRows = mapDropTable.getElementsByTagName("tr");
 
   let result = {};
